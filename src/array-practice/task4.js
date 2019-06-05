86f8e1ae8c118d02163250674082497b0fd82e00
vitaly_335 <47710161+samihada35@users.noreply.github.com>
2019-06-05 16:05:20 +0300

    Revert "Revert "task1Arr""
    This reverts commit b2d5c7f086e6e02c9213412ac2372537aca6306a.

--- src/array-practice/task4.js
+++ src/array-practice/task4.js
@@ -7,3 +7,13 @@
  * console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
  * console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
  */
+
+function union (arr1,arr2) {
+  var arr1;
+  var arr2;
+  var newArr = arr1.concat(arr2);
+  let setArr = new Set(newArr);
+  // eslint-disable-next-line no-console
+  console.log(setArr);
+}
+union([5, 1, 3, 3, 4], [1, 3, 4]);
